9ed216067ccbfe5c72f1a7e6c7b3e01aad57cc9a
Lofe <30690883+iamlofe@users.noreply.github.com>
2018-05-26 19:06:15 +0300

    tamplate without line

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$('.mobile_menu_click').click(function() {
+  $('.list-menu').fadeToggle();
+});
+const navs = document.querySelectorAll('.nav-select');
+
+navs.forEach(nav => {
+  nav.addEventListener('click', e => {
+    const name = nav.classList
+      .toString()
+      .split(' ')
+      .filter(className => className != 'nav-select')
+      .join('');
+    if (name) {
+      $('.toggleable').fadeOut();
+      $('.list-menu').fadeOut();
+      $(`.${name}.toggleable`).fadeIn();
+    }
+  });
+});
+
+$(document).mouseup(function(e) {
+  var container = $('.toggleable');
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    container.fadeOut();
+  }
+});
+
+$('#exit').click(function() {
+  $('.list-menu').fadeToggle();
+});
